a2fd0300e6c5c7420c27303873e67a1eaf7923ff
Rosapetals <52610978+Rosapetals@users.noreply.github.com>
2022-12-25 18:31:38 -0600

    born

---
+++ src/main/java/core/Florial.java
@@ -0,0 +1,123 @@
+package core;
+
+import co.aikar.commands.PaperCommandManager;
+import io.github.nosequel.menu.MenuHandler;
+import lombok.Getter;
+import lombok.SneakyThrows;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Getter
+public final class Florial extends JavaPlugin {
+
+
+    @Getter
+    private static Florial instance;
+
+
+    final FileConfiguration config2 = this.getConfig();
+
+
+    @SneakyThrows
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        setupCommands();
+        setupListeners();
+        new MenuHandler(this);
+        enableRecipes();
+        setupManagers();
+
+        // test
+
+    }
+
+
+    @Override
+    public void onDisable() {
+        //if (!(Cooldown.getCooldownMap("powercooldown1") == null)) Cooldown.getCooldownMap("powercooldown1").clear();
+        saveConfig();
+    }
+
+    private void setupManagers() {
+        //if (!(Cooldown.getCooldownMap("powercooldown1") == null)) Cooldown.getCooldownMap("powercooldown1").clear();
+
+    }
+
+    private void setupListeners() {
+    }
+
+    private void enableRecipes() {
+    }
+
+    private void registerRecipes(String thekey, Boolean isShaped, String column1, String column2, String column3,
+                                 ItemStack recipeitem1, ItemStack recipeitem2, ItemStack recipeitem3, ItemStack recipeitem4,
+                                 ItemStack recipeitem5, ItemStack recipeitem6, ItemStack recipeitem7, ItemStack recipeitem8, ItemStack recipeitem9, ItemStack output) {
+        NamespacedKey key = new NamespacedKey(this, thekey);
+        ShapelessRecipe therecipe = new ShapelessRecipe(key, output);
+        ShapedRecipe therecipe2 = new ShapedRecipe(key, output);
+        if (isShaped == true) {
+            therecipe2.shape(column1, column2, column3);
+            if (!(recipeitem1.getType() == Material.AIR))
+                therecipe2.setIngredient('1', new RecipeChoice.ExactChoice(recipeitem1));
+            if (!(recipeitem2.getType() == Material.AIR))
+                therecipe2.setIngredient('2', new RecipeChoice.ExactChoice(recipeitem2));
+            if (!(recipeitem3.getType() == Material.AIR))
+                therecipe2.setIngredient('3', new RecipeChoice.ExactChoice(recipeitem3));
+            if (!(recipeitem4.getType() == Material.AIR))
+                therecipe2.setIngredient('4', new RecipeChoice.ExactChoice(recipeitem4));
+            if (!(recipeitem5.getType() == Material.AIR))
+                therecipe2.setIngredient('5', new RecipeChoice.ExactChoice(recipeitem5));
+            if (!(recipeitem6.getType() == Material.AIR))
+                therecipe2.setIngredient('6', new RecipeChoice.ExactChoice(recipeitem6));
+            if (!(recipeitem7.getType() == Material.AIR))
+                therecipe2.setIngredient('7', new RecipeChoice.ExactChoice(recipeitem7));
+            if (!(recipeitem8.getType() == Material.AIR))
+                therecipe2.setIngredient('8', new RecipeChoice.ExactChoice(recipeitem8));
+            if (!(recipeitem9.getType() == Material.AIR))
+                therecipe2.setIngredient('9', new RecipeChoice.ExactChoice(recipeitem9));
+            Bukkit.removeRecipe(key);
+            Bukkit.addRecipe(therecipe2);
+            return;
+        } else {
+            if (!(recipeitem1.getType() == Material.AIR))
+                therecipe.addIngredient(new RecipeChoice.ExactChoice(recipeitem1));
+            if (!(recipeitem2.getType() == Material.AIR))
+                therecipe.addIngredient(new RecipeChoice.ExactChoice(recipeitem2));
+            if (!(recipeitem3.getType() == Material.AIR))
+                therecipe.addIngredient(new RecipeChoice.ExactChoice(recipeitem3));
+            if (!(recipeitem4.getType() == Material.AIR))
+                therecipe.addIngredient(new RecipeChoice.ExactChoice(recipeitem4));
+            if (!(recipeitem5.getType() == Material.AIR))
+                therecipe.addIngredient(new RecipeChoice.ExactChoice(recipeitem5));
+            if (!(recipeitem6.getType() == Material.AIR))
+                therecipe.addIngredient(new RecipeChoice.ExactChoice(recipeitem6));
+            if (!(recipeitem7.getType() == Material.AIR))
+                therecipe.addIngredient(new RecipeChoice.ExactChoice(recipeitem7));
+            if (!(recipeitem8.getType() == Material.AIR))
+                therecipe.addIngredient(new RecipeChoice.ExactChoice(recipeitem8));
+            if (!(recipeitem9.getType() == Material.AIR))
+                therecipe.addIngredient(new RecipeChoice.ExactChoice(recipeitem9));
+            Bukkit.removeRecipe(key);
+            Bukkit.addRecipe(therecipe);
+            return;
+        }
+    }
+
+
+    public void updateScoreBoards() {
+    }
+
+    private void setupCommands() {
+        PaperCommandManager manager = new PaperCommandManager(this);
+
+    }
+}
